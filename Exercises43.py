4b63b79e60df48ad62fa17fec356e198575312a2
EdwinTa <47590630+EdwinTa@users.noreply.github.com>
2019-02-14 14:17:27 +0800

    添加测试

--- Exercises43.py
+++ Exercises43.py
@@ -6,12 +6,16 @@
 求输入数的平方，如果平方算后小于50则退出
 '''
 
-num=int(input("input a num:"))
 
 
-if num*num < 50:
-    print("输入数的平方为：%d"%(num*num))
-elif num*num == 50:
-    print("输入数的平方为：%d，等于50" %(num * num))
-else:
-    print("输入数的平方为：%d，大于" %( num * num))
+try:
+    num = int(input("input a num:"))
+    if num*num < 50:
+        print("输入数的平方为：%d"%(num*num))
+    elif num*num == 50:
+        print("输入数的平方为：%d，等于50" %(num * num))
+    else:
+        print("输入数的平方为：%d，大于50" %( num * num))
+
+except:
+    print('输入错误')
